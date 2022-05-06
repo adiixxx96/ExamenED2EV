2147825076cf42cfc2e15817e68e3faa2d44c1f8
adiixxx96 <ada9659@gmail.com>
2022-05-06 18:43:20 +0200

    Crear clase partida, constructor y métodos

---
+++ src/main/java/com/sanvalero/Partida.java
@@ -0,0 +1,34 @@
+package com.sanvalero;
+
+import java.util.ArrayList;
+
+public class Partida {
+    private String nombre;
+    private ArrayList<Jugador> listaJugadores;
+    private Baraja baraja;
+    private int puntuacionJugadores;
+
+    public Partida(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void addJugador(Jugador j) {
+
+    }
+
+    public void jugarPartida(int turnos) {
+
+    }
+
+    public void jugarRonda() {
+
+    }
+
+    public void comprobarGanadorRonda() {
+
+    }
+
+    public void comprobarGanadorPartida() {
+
+    }
+}
