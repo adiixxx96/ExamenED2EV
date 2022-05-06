2a5c07ad6c5944219c18359dc6bd2430b35cd754
adiixxx96 <ada9659@gmail.com>
2022-05-06 18:38:52 +0200

    Crear clase jugador, constructor y métodos

---
+++ src/main/java/com/sanvalero/Jugador.java
@@ -0,0 +1,29 @@
+package com.sanvalero;
+
+import java.util.ArrayList;
+
+public class Jugador {
+    private String nombre;
+    private ArrayList<Carta> misCartas;
+    private int puntos;
+
+    public Jugador(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void preguntarMovimiento() {
+
+    }
+
+    public void addCarta() {
+
+    }
+
+    public void imprimirCartas() {
+
+    }
+
+    public void resetearRonda() {
+
+    }
+}
